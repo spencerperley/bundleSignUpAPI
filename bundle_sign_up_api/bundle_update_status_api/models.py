abd80787197a700a03df1ae02ffedd186463a8c1
spencerperley <63747892+spencerperley@users.noreply.github.com>
2023-12-26 11:26:04 -0700

    feat: created activate autoDelivery and migrated to database

--- bundle_sign_up_api/bundle_update_status_api/models.py
+++ bundle_sign_up_api/bundle_update_status_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class ActivateAutoDelivery(models.Model):
+    unit = models.IntegerField()
+    email = models.EmailField()
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    requestTime = models.DateField()
