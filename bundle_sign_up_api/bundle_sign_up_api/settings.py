1be714625f4a186e3e36f9615fa0c0c1e24eb9e1
spencerperley <63747892+spencerperley@users.noreply.github.com>
2023-12-26 10:59:01 -0700

    feat: added my app to installed apps

--- bundle_sign_up_api/bundle_sign_up_api/settings.py
+++ bundle_sign_up_api/bundle_sign_up_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bundle_update_status_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
